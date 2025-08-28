67834bc5b064e86aabcd17f1f39e2054b6ce2d2c
Kosuke Mizumura <kosuke@soundonlive.com>
2025-08-29 01:53:09 +0900

    fix

--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -1,7 +1,7 @@
-import { GridTileImage } from 'components/grid/tile';
-import { getCollectionProducts } from 'lib/shopify';
-import type { Product } from 'lib/shopify/types';
-import Link from 'next/link';
+import { GridTileImage } from 'components/grid/tile'
+import { getCollectionProducts } from 'lib/shopify'
+import type { Product } from 'lib/shopify/types'
+import Link from 'next/link'
 
 function ThreeItemGridItem({
   item,
